6a2a5e8bb096c83f24bc51471c04240c97517aed
Mayank-MP05 <mayank5pande@gmail.com>
2021-02-24 18:09:08 +0530

    Navbar Pic Icon Updated

---
+++ js/account/account-ui-updator.js
@@ -0,0 +1,65 @@
+var user = firebase.auth().currentUser;
+var name, email, photoUrl, uid, emailVerified;
+
+$("#dpUpdater").on("change", (e) => {
+  //   console.log(e.target.value);
+  $("#AvatarImg").attr("src", imgSrcArr[parseInt(e.target.value)]);
+});
+
+firebase.auth().onAuthStateChanged(function (user) {
+  if (user) {
+    // User is signed in.
+    // window.alert("hi" + " " + user.uid);
+    var userid = user.uid;
+    var nam = user.displayName;
+    joinInTriggerBtn.hide();
+    dropdownTriggerBtn.show();
+
+    name = user.displayName;
+    email = user.email;
+    photoUrl = user.photoURL;
+    emailVerified = user.emailVerified;
+    uid = user.uid;
+    console.log(name, email, photoUrl);
+    $("#emailInput").val(email);
+    if (name) {
+      $("#nameInput").val(name);
+    }
+    if (photoUrl) {
+      $("#AvatarImg").attr("src", imgSrcArr[parseInt(photoUrl)]);
+      $("#dpUpdater").val(photoUrl);
+    }
+
+    $("#SaveChangesBtn").on("click", () => {
+      user
+        .updateProfile({
+          displayName: $("#nameInput").val(),
+          photoURL: parseInt($("#dpUpdater").val()),
+        })
+        .then(function () {
+          console.log("Updation done successful!");
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    });
+    $("#ResetBtn").on("click", () => {
+      if (name) {
+        $("#nameInput").val(name);
+      }
+      if (photoUrl) {
+        $("#AvatarImg").attr("src", imgSrcArr[parseInt(photoUrl)]);
+      }
+    });
+
+    /*
+     */
+  } else {
+    // No user is signed in.
+    /*window.location.replace("http://127.0.0.1:5500/login.html");*/
+    // window.location.href = "learn.html";
+    // alert("No user signed in");
+    joinInTriggerBtn.show();
+    dropdownTriggerBtn.hide();
+  }
+});
--- js/firebase/auth-ui-controllers.js
+++ js/firebase/auth-ui-controllers.js
@@ -4,6 +4,18 @@ const joinInTriggerBtn = $("#Join-In-Trigger-Button");
 const dropdownTriggerBtn = $("#logged-In-Dropdown-Trigger-Btn");
 const logoutBtn = $("#logout-trigger-btn");
 logoutBtn.click(() => logout());
+const imgSrcArr = [
+  "./img/users/user (1).png",
+  "./img/users/user (2).png",
+  "./img/users/user (3).png",
+  "./img/users/user (4).png",
+  "./img/users/user (5).png",
+  "./img/users/user (6).png",
+  "./img/users/user (7).png",
+  "./img/users/user (8).png",
+  "./img/users/user (9).png",
+];
+
 firebase.auth().onAuthStateChanged(function (user) {
   if (user) {
     // User is signed in.
@@ -12,6 +24,9 @@ firebase.auth().onAuthStateChanged(function (user) {
     var nam = user.displayName;
     joinInTriggerBtn.hide();
     dropdownTriggerBtn.show();
+    console.log(user);
+    if (user.photoURL)
+      $("#UserImg").attr("src", imgSrcArr[parseInt(user.photoURL)]);
   } else {
     // No user is signed in.
     /*window.location.replace("http://127.0.0.1:5500/login.html");*/
