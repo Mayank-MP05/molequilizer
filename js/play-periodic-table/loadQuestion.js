6ecf15de26ee85b03fd7c4ff06421ccb7bddadd1
Mayank-MP05 <mayank5pande@gmail.com>
2021-03-05 11:44:59 +0530

    Styling fixes on Submit Form

--- js/play-periodic-table/loadQuestion.js
+++ js/play-periodic-table/loadQuestion.js
@@ -1,6 +1,3 @@
-let quiz = quizesDB[0];
-console.log(quiz);
-
 $("#a");
 $("#b");
 $("#c");
@@ -20,8 +17,9 @@ const PeriodicGameState = {
   optionB: "",
   optionC: "",
   optionD: "",
-  correctAns: "",
-  selctedAns: "",
+  answerElement: "",
+  correctAns: 0,
+  selctedAns: 0,
 };
 
 // $(".answer").on("click", (e) => {
@@ -33,6 +31,96 @@ const PeriodicGameState = {
 //   $("#a").prop("checked", true);
 // });
 
-const UIupdator = () => {
+$("#quiz-form input").on("change", function () {
+  alert($("input[name='answer']:checked", "#quiz-form").val());
+});
+
+const GetNewQuestion = () => {
   console.log("UI Updator Called...");
+  PeriodicGameState.quizArr = quizesDB[0];
+  let quiz = quizesDB[0];
+  console.log(quiz);
+  let ansIndex = Math.floor(Math.random() * quiz.length);
+  PeriodicGameState.answerElement = PeriodicGameState.quizArr[ansIndex];
+  //   console.log(ansIndex);
+
+  //Question string making logic
+  let questionStr = ``;
+  quiz.forEach((el, index) => {
+    if (index != 0 && index != quiz.length) {
+      questionStr += ", ";
+    }
+    if (index == ansIndex) {
+      questionStr += `?`;
+    } else {
+      questionStr += `${el}`;
+    }
+  });
+  console.log(questionStr);
+  PeriodicGameState.question = questionStr;
+
+  //Option Rendering Logic
+  let atomsDB = atomic_data;
+  let optionAtom;
+
+  for (let i = 0; i <= 3; i++) {
+    optionAtom = atomsDB[`${Math.floor(Math.random() * 118)}`];
+    switch (i) {
+      case 0:
+        PeriodicGameState.optionA = optionAtom.atomic_symbol;
+
+      case 1:
+        PeriodicGameState.optionB = optionAtom.atomic_symbol;
+
+      case 2:
+        PeriodicGameState.optionC = optionAtom.atomic_symbol;
+
+      case 3:
+        PeriodicGameState.optionD = optionAtom.atomic_symbol;
+    }
+  }
+  ansIndex = Math.floor(Math.random() * 4);
+  switch (ansIndex) {
+    case 0:
+      PeriodicGameState.optionA = PeriodicGameState.answerElement;
+      break;
+    case 1:
+      PeriodicGameState.optionB = PeriodicGameState.answerElement;
+      break;
+    case 2:
+      PeriodicGameState.optionC = PeriodicGameState.answerElement;
+      break;
+    case 3:
+      PeriodicGameState.optionD = PeriodicGameState.answerElement;
+      break;
+  }
+  PeriodicGameState.correctAns = ansIndex;
+
+  console.log(PeriodicGameState);
 };
+
+const UIupdator = () => {
+  $("#question").html(PeriodicGameState.question);
+  //Option Updation
+  periodictabledata.forEach((el) => {
+    if (el.symbol == PeriodicGameState.optionA) {
+      $("#a_text").html(`${el.symbol} - ${el.name}`);
+    }
+    if (el.symbol == PeriodicGameState.optionB) {
+      $("#b_text").html(`${el.symbol} - ${el.name}`);
+    }
+    if (el.symbol == PeriodicGameState.optionC) {
+      $("#c_text").html(`${el.symbol} - ${el.name}`);
+    }
+    if (el.symbol == PeriodicGameState.optionD) {
+      $("#d_text").html(`${el.symbol} - ${el.name}`);
+    }
+  });
+  //   $("#a_text").html(PeriodicGameState.optionA);
+  //   $("#b_text").html(PeriodicGameState.optionB);
+  //   $("#c_text").html(PeriodicGameState.optionC);
+  //   $("#d_text").html(PeriodicGameState.optionD);
+};
+
+GetNewQuestion();
+UIupdator();
